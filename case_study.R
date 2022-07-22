aa30f9c5e631ab4c7cc081cc59cfa9422581b2a3
S Lee <sanlee8495@gmail.com>
2022-07-22 11:29:52 -0700

    7/22/22

--- case_study.R
+++ case_study.R
@@ -50,5 +50,33 @@ summary %>%
 
 
 
+######################################################################################################
+############################################### Shiny ################################################
+######################################################################################################
+
+prod_codes <- setNames(products$prod_code, products$title)
+
+
+
+ui <- fluidPage(
+  fluidRow(
+    column(6,
+           selectInput("code", "Product", choices = prod_codes))
+  ),
+  fluidRow(
+    column(4, tableOutput("diag")),
+    column(4, tableOutput("body_part")),
+    column(4, tableOutput("location"))
+  ),
+  fluidRow(
+    column(12, plotOutput("age_sex"))
+  )
+)
+
+
+
+
+
+
 
 
