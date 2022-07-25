8f66e86fa5c88c1c4ac8980b6a92b9de9673d9d2
S Lee <sanlee8495@gmail.com>
2022-07-25 13:58:25 -0700

    7/25/22

--- case_study.R
+++ case_study.R
@@ -74,7 +74,32 @@ ui <- fluidPage(
 )
 
 
-
+server <- function(input, output, session){
+  selected <- reactive(injuries %>% dplyr::filter(prod_code == input$code))
+  
+  output$diag <- renderTable(
+    selected() %>% dplyr::count(diag, wt = weight, sort = TRUE))
+  
+  output$body_part <- renderTable(
+    selected() %>% dplyr::count(body_part, wt = weight, sort =  TRUE))
+  
+  output$location <- renderTable(
+    selected() %>% dplyr::count(location, wt = weight, sort = TRUE))
+  
+  summary <- reactive({
+    selected() %>% 
+      dplyr::count(age, sex, wt = weight) %>% 
+      dplyr::left_join(population, by = c("age", "sex")) %>% 
+      dplyr::mutate(rate = n / population * 10000)
+    })
+  
+  output$age_sex <- renderPlot({
+    summary() %>% 
+      ggplot2::ggplot(mapping = aes(x = age, y = n, color = sex)) +
+      ggplot2::geom_line() +
+      ggplot2::labs(y = "Estimated number of injuries")
+  }, res = 96)
+}
 
 
 
